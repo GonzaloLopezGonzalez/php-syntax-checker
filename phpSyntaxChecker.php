7607b0c2b78232c30fe583757201a64039ebbe12
Gonzalo López <lopezgonzalez@hotmail.com>
2024-12-27 13:56:19 +0100

    Update phpSyntaxChecker.php

--- phpSyntaxChecker.php
+++ phpSyntaxChecker.php
@@ -7,10 +7,10 @@ class phpSyntaxChecker
 
     public function __construct()
     {
+        $this->filesWithSyntaxErrors = array();
        if (!$this->checkIfSyntaxCheckerCommandCanBeExecuted()){
-         throw new ErrorException('Es necesario poder ejecutar el comando "system" de php');
+         throw new ErrorException('Es necesario poder ejecutar el comando "exec" de php');
        }
-
     }
 
     private function checkIfSyntaxCheckerCommandCanBeExecuted(): bool
@@ -19,34 +19,35 @@ class phpSyntaxChecker
        return (in_array('exec', $disabledFunctions)) ? false : true;
     }
 
-    public function checkPhpFileSyntax(string $file)
+    public function checkPhpFileSyntax(string $fileName)
     {
         $result = array();
-        $comando = 'php -l ' . $file;
-        exec($comando, $result);
-        if (false === strpos($result[0], 'No syntax errors detected in')){
+        $return = 0;
+        exec("php -ln {$fileName}", $result, $return);
+        if (255 === $return){
             $this->filesWithSyntaxErrors[] = $result[1];
         }
     }
 
-
     public function analyzePhpFilesSyntax(string $ruta = __DIR__, bool $checkRoot = true)
     {
-        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator("C:/xampp/htdocs/pruebas"));
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($ruta));
         foreach ($iterator as $archivo) {
-            if ($archivo->isDir()) {
+            $posPunto = strpos($archivo, '\.');
+            if ($archivo->isDir() || $posPunto !== false) {
                 continue; // Saltar directorios
             }
             if ('php' === $archivo->getExtension()){
                $this->checkPhpFileSyntax($archivo->getPathname());
             }
         }
+        unset($iterator);
         $this->getResult();
     }
 
     private function getResult(): void {
         foreach ($this->filesWithSyntaxErrors as $message){
-            echo $message."\n";
+           echo $message."\n";
         }
     }
 }
